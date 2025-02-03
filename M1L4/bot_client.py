c9faf4abee7a48dc5a1beba9db7f4364024231ef
Yonca <73234933+yoncasen@users.noreply.github.com>
2025-02-03 21:04:35 +0300

    Add files via upload

---
+++ M1L4/bot_client.py
@@ -0,0 +1,26 @@
+import discord
+from bot_token import token
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptık')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$hello'):
+        await message.channel.send(f'Merhaba! Ben {client.user}, bir Discord sohbet botuyum!')
+    elif  message.content.startswith('$heh'):
+        if len(message.content) > 4:
+            count_heh = int(message.content[4:])
+        else:
+            count_heh = 5
+        await message.channel.send("he" * count_heh)
+        
+client.run(token)
